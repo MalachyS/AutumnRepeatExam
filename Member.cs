2e3e828f6376d10d3a1f9ca9cd74e5e7b238e486
MalachyS <S00221188@atu.ie>
2023-08-18 09:52:35 +0100

    Implemented GetAge method & completed team class & member class

--- Member.cs
+++ Member.cs
@@ -1,34 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace AutumnRepeatExam
+internal class Member : IComparable<Member>
 {
-    internal class Member
+    // Properties
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DOB { get; set; }
+
+    // Constructors
+    public Member(string firstName, string lastName, DateTime dOB)
     {
-        //properties
-        public string FirstName { get; set; }
-        public string LastName { get; set; }    
-        public DateTime DOB { get; set; }
-        //Constructors
-        public Member(string firstName, string lastName, DateTime dOB)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            DOB = dOB;
-        }
-        public Member() { }
-
-        
-        public override string ToString()
-        {
-            return $"{LastName}, {FirstName} - {DOB}"; //This makes the data actually display in the LBX
-        }
-
-
-
+        FirstName = firstName;
+        LastName = lastName;
+        DOB = dOB;
     }
-    
+
+    public Member() { }
+
+    // Method to calculate age
+    public int GetAge()
+    {
+        DateTime currentDate = DateTime.Now;
+        int age = currentDate.Year - DOB.Year;
+
+        if (currentDate.Month < DOB.Month || (currentDate.Month == DOB.Month && currentDate.Day < DOB.Day))
+        {
+            age--;
+        }
+
+        return age;
+    }
+
+    public int CompareTo(Member other)
+    {
+        if (other == null)
+        {
+            return 1; // Null objects are considered greater
+        }
+
+        return string.Compare(this.LastName, other.LastName, StringComparison.Ordinal);
+    }
+
+    // Override ToString method
+    public override string ToString()
+    {
+        int age = GetAge();
+        return $"{LastName}, {FirstName} - {DOB:MM/dd/yyyy} ({age})";
+    }
 }
---
+++ Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnRepeatExam
+{
+    internal class Team
+    {
+        public string Teamname { get; set; }
+        public List<Member> players { get; set; }
+    }
+}
